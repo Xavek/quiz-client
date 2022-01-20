6d1c31705ab40d17e0d76e872b0de2430fcf352f
gautam2002 <theofficalgautam200@gmail.com>
2022-01-20 11:42:17 +0545

    major ui changes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -28,25 +28,29 @@ const Signup = () => {
   };
   return (
     <>
-      <p className="mt-8 text-center px-2 py-2 text-lg font-semibold font-sans ">
+      <p className="mt-8 text-center px-2 py-2 text-lg font-semibold font-mono ">
         First Time Sign Up To Play a Game. OR{" "}
         <Link to="/login">
-          <button className="mx-2 my-x px-2 py-2 bg-slate-400 text-lg font-mono  font-semibold rounded-lg">
+          <button className="mx-1 my-1 px-2 py-1 bg-slate-400 text-lg font-mono  font-semibold rounded-lg">
             Login
           </button>
         </Link>
       </p>
-      <div className="mt-20 grid place-items-center h-64">
-        <label htmlFor="username">User Name (4 to 8 characters):</label>
+      <div className="mt-20 grid place-items-center h-64 font-mono">
+        <label htmlFor="username" className="text-2xl">
+          User Name (4 to 8 char):
+        </label>
         <input
           ref={refUsernameValue}
           type="text"
           id="username"
           name="username"
           required
-          className="block bg-white w-50 border border-gray-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          className="block bg-white w-50 border border-gray-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-2xl"
         ></input>
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password" className="text-2xl">
+          Password
+        </label>
 
         <input
           ref={refPassValue}
@@ -54,11 +58,11 @@ const Signup = () => {
           id="password"
           name="password"
           required
-          className="block  bg-white w-50 border border-gray-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          className="block  bg-white w-50 border border-gray-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-2xl"
         ></input>
         <button
           onClick={handleSubmit}
-          className="my-2 py-2 px-4  bg-slate-400 text-white-800 font-semibold text-lg transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 rounded-lg border-l-8 border-zinc-400"
+          className="my-2 py-2 px-4  bg-slate-400 text-white-800 font-semibold transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 rounded-lg border-l-8 border-zinc-400 text-2xl"
         >
           Submit
         </button>
